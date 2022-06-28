6e1c0de0b5433897b81d0894b5cb99709471679e
Christina Li <cyli@fb.com>
2022-06-28 13:05:22 -0700

    first commit

---
+++ InstagramClone/AppDelegate.h
@@ -0,0 +1,14 @@
+//
+//  AppDelegate.h
+//  InstagramClone
+//
+//  Created by Christina Li on 6/28/22.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ InstagramClone/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  InstagramClone
+//
+//  Created by Christina Li on 6/28/22.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
