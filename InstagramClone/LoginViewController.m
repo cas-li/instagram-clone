335298947ac5fa7543d5415a1f8b012d4e4fdb76
Christina Li <cyli@fb.com>
2022-06-28 15:26:00 -0700

    implemented login with segue

--- InstagramClone/LoginViewController.m
+++ InstagramClone/LoginViewController.m
@@ -42,8 +42,30 @@
     }];
 }
 
+- (void)loginUser {
+    NSString *username = self.usernameField.text;
+    NSString *password = self.passwordField.text;
+    
+    [PFUser logInWithUsernameInBackground:username password:password block:^(PFUser * user, NSError *  error) {
+        if (error != nil) {
+            NSLog(@"User log in failed: %@", error.localizedDescription);
+        } else {
+            NSLog(@"User logged in successfully");
+            [self performSegueWithIdentifier:@"loginSegue" sender:nil];
+            
+            // display view controller that needs to shown after successful login
+        }
+    }];
+}
+
 - (IBAction)didTapSignup:(id)sender {
     [self registerUser];
+}
+
+
+- (IBAction)didTapLogin:(id)sender {
+    
+    [self loginUser];
 }
 
 
