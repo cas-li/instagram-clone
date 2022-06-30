2d1e4aac719862e212dea6931a49f1d4cef07f75
Cas Li <tina.li003@gmail.com>
2022-06-29 17:46:40 -0700

    fixed date bug

--- InstagramClone/DetailsViewController.m
+++ InstagramClone/DetailsViewController.m
@@ -19,24 +19,14 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
-    self.caption.text = self.post[@"caption"];
-    self.postPicture.file = self.post[@"image"];
+    self.caption.text = self.post.caption;
+    self.postPicture.file = self.post.image;
     [self.postPicture loadInBackground];
     
-//    self.timestamp.text = self.post[@"createdAt"];
     NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-//    // Configure the input format to parse the date string
-//    formatter.dateFormat = @"E MMM d HH:mm:ss Z y";
-//    // Convert String to Date
-//    NSDate *date = [formatter dateFromString:createdAtOriginalString];
-//    self.createdAtDate = date;
-//    // Configure output format
     formatter.dateStyle = NSDateFormatterShortStyle;
     formatter.timeStyle = NSDateFormatterNoStyle;
-//    // Convert Date to String
-    self.timestamp.text = [formatter stringFromDate:self.post[@"createdAt"]];
-    NSLog(@"Value of createdAt = %@", self.post[@"createdAt"]);
-    NSLog(@"Value of timestamp = %@", self.timestamp.text);
+    self.timestamp.text = [formatter stringFromDate:self.post.createdAt];
     
 }
 
